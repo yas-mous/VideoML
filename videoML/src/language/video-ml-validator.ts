50cc4940466bb2e8daa2bd8180573c105dbcdb7a
Anna <annadiplacido3@gmail.com>
2024-12-26 21:24:03 +0100

    init : init langium project

---
+++ videoML/src/language/video-ml-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { VideoMlAstType, Person } from './generated/ast.js';
+import type { VideoMlServices } from './video-ml-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: VideoMlServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.VideoMlValidator;
+    const checks: ValidationChecks<VideoMlAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class VideoMlValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
---
+++ videoML/src/setupExtended.ts
@@ -0,0 +1,58 @@
+import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
+import { configureWorker, defineUserServices } from './setupCommon.js';
+
+export const setupConfigExtended = (): UserConfig => {
+    const extensionFilesOrContents = new Map();
+    extensionFilesOrContents.set('/language-configuration.json', new URL('../language-configuration.json', import.meta.url));
+    extensionFilesOrContents.set('/video-ml-grammar.json', new URL('../syntaxes/video-ml.tmLanguage.json', import.meta.url));
+
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'extended',
+                languageId: 'video-ml',
+                code: `// videoML is running in the web!`,
+                useDiffEditor: false,
+                extensions: [{
+                    config: {
+                        name: 'video-ml-web',
+                        publisher: 'generator-langium',
+                        version: '1.0.0',
+                        engines: {
+                            vscode: '*'
+                        },
+                        contributes: {
+                            languages: [{
+                                id: 'video-ml',
+                                extensions: [
+                                    '.video-ml'
+                                ],
+                                configuration: './language-configuration.json'
+                            }],
+                            grammars: [{
+                                language: 'video-ml',
+                                scopeName: 'source.video-ml',
+                                path: './video-ml-grammar.json'
+                            }]
+                        }
+                    },
+                    filesOrContents: extensionFilesOrContents,
+                }],                
+                userConfiguration: {
+                    json: JSON.stringify({
+                        'workbench.colorTheme': 'Default Dark Modern',
+                        'editor.semanticHighlighting.enabled': true
+                    })
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeExtended = async (htmlElement: HTMLElement) => {
+    const userConfig = setupConfigExtended();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.initAndStart(userConfig, htmlElement);
+};
