9bcfb6ccab57cabca1dd61ed41313842e555a525
Anna <annadiplacido3@gmail.com>
2024-12-28 15:13:33 +0100

    feat : update grammar

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ voir dans langium-quickstart.md
 ```bash
 npm run cli ..\demo\test2.vml 
 ```
+
+### Exemple de grammaire possible 
+```bash
+timeline App {
+    ---
+    |'video1.mp4'
+    |'video2.mp4', begin: 0, end: 10
+    |'video3.mp4', begin: 10, end: 20
+    ---
+    |'video4.mp4'
+    |'video5.mp4', begin: 30, end: 40
+    |'video6.mp4', begin: 40, end: 50
+    ---
+    |'video7.mp4'
+}
+
+```
--- videoML/src/cli/generator.ts
+++ videoML/src/cli/generator.ts
@@ -1,4 +1,4 @@
-import { expandToNode, toString } from 'langium/generate';
+import { CompositeGeneratorNode, toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { extractDestinationAndName } from './cli-util.js';
@@ -8,10 +8,8 @@ export function generateJavaScript(timeline: TimeLine, filePath: string, destina
     const data = extractDestinationAndName(filePath, destination);
     const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
 
-    const fileNode = expandToNode`
-        "use strict";
-
-    `
+    const fileNode = new CompositeGeneratorNode();
+    compile(timeline,fileNode)
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
@@ -19,3 +17,8 @@ export function generateJavaScript(timeline: TimeLine, filePath: string, destina
     fs.writeFileSync(generatedFilePath, toString(fileNode));
     return generatedFilePath;
 }
+
+function compile(timeline: TimeLine, fileNode: CompositeGeneratorNode) {
+    throw new Error('Function not implemented.');
+}
+
