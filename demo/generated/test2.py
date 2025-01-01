26acca842f861bd2437a4077d7ca31f91925300d
Anna <annadiplacido3@gmail.com>
2025-01-01 14:04:33 +0100

    update video

--- demo/generated/test2.py
+++ demo/generated/test2.py
@@ -1,13 +1,10 @@
 from moviepy import *
 
-layer_0 = CompositeVideoClip([
-    VideoFileClip("videos/video1.mp4"),
-    VideoFileClip("videos/video2.mp4"),
-])
+VideoFileClip("videos/video1.mp4")
 VideoFileClip("videos/video2.mp4")
 
 final_video = concatenate_videoclips([
-    layer_0,
+    VideoFileClip("videos/video1.mp4"),
     VideoFileClip("videos/video2.mp4"),
 ])
 final_video.write_videofile("myVideo.mp4", fps=24)
