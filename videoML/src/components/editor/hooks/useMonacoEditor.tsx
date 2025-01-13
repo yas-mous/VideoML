df4fa6d3568b70eb011937b861cf3582004283d3
Anna <annadiplacido3@gmail.com>
2025-01-14 00:40:44 +0100

    feat : add parsing with ast !!!!!!

--- videoML/src/components/editor/hooks/useMonacoEditor.tsx
+++ videoML/src/components/editor/hooks/useMonacoEditor.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper";
-import { setupConfigExtended  } from "../../../setupExtended.ts";
+import { setupConfigExtended } from "../../../setupExtended.ts";
 import { configureMonacoWorkers } from "../../../setupCommon.ts";
 
 export const useMonacoEditor = (setCode: (code: string) => void) => {
@@ -14,18 +14,43 @@ export const useMonacoEditor = (setCode: (code: string) => void) => {
 
         if (!initialized.current) {
             wrapper.current.initAndStart(userConfig, refHtml.current).then(() => {
-                const editor = wrapper.current.getEditor();
-                editor?.onDidChangeModelContent(() => {
-                    const newCode = editor.getValue() || "";
-                    setCode(newCode); 
-                });
+                const languageClient = wrapper.current.getLanguageClient();
+
+                if (languageClient) {
+                    let running = false; 
+                    let timeout: number | null = null; 
+
+                    languageClient.onNotification('browser/DocumentChange', (change) => {
+                        console.log("Document change notification received:", change);
+                        if (running) {
+                            return;
+                        }
+
+                        if (timeout) {
+                            clearTimeout(timeout);
+                        }
+
+                        timeout = window.setTimeout(async () => {
+                            running = true;
+
+                            const ast = JSON.parse(change.content);
+
+                            console.log("Document URI:", change.uri);
+                            console.log("Serialized AST:", ast);
+                            console.log("Diagnostics:", change.diagnostics);
+                            // updateUIWithAST(ast);
+
+                            running = false;
+                        }, 200); 
+                    });
+                }
             });
 
             initialized.current = true;
         }
 
         return () => {
-            wrapper.current.dispose(); 
+            wrapper.current.dispose();
         };
     }, [setCode]);
 
--- videoML/src/language/main-browser.ts
+++ videoML/src/language/main-browser.ts
@@ -1,7 +1,9 @@
-import { EmptyFileSystem } from 'langium';
+import { DocumentState, EmptyFileSystem } from 'langium';
 import { startLanguageServer } from 'langium/lsp';
-import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection, Diagnostic, NotificationType } from 'vscode-languageserver/browser.js';
 import { createVideoMlServices } from './video-ml-module.js';
+
+import { TimeLine } from './generated/ast.js';
 
 declare const self: DedicatedWorkerGlobalScope;
 
@@ -10,6 +12,28 @@ const messageWriter = new BrowserMessageWriter(self);
 
 const connection = createConnection(messageReader, messageWriter);
 
-const { shared } = createVideoMlServices({ connection, ...EmptyFileSystem });
+const { shared,VideoMl } = createVideoMlServices({ connection, ...EmptyFileSystem });
 
 startLanguageServer(shared);
+
+type DocumentChange = { uri: string, content: string, diagnostics: Diagnostic[] };
+const documentChangeNotification = new NotificationType<DocumentChange>('browser/DocumentChange');
+
+const jsonSerializer = VideoMl.serializer.JsonSerializer;
+shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Validated, documents => {
+
+    for (const document of documents) {
+        const model = document.parseResult.value as TimeLine;
+        const serializedAst = jsonSerializer.serialize(model, {
+            sourceText: true, 
+            textRegions: true, 
+        });
+       
+
+        connection.sendNotification(documentChangeNotification, {
+            uri: document.uri.toString(),
+            content: serializedAst,
+            diagnostics: document.diagnostics ?? []
+        });
+    }
+});
