da964cfdf1bdcd857cda35a737648b23f5e0ae83
EmmanuelleAD <emmanuelleadote@gmail.com>
2025-01-19 19:26:44 +0100

    scenario2 fin correction vml

--- demo/generated/customclipexemple.py
+++ demo/generated/customclipexemple.py
@@ -6,30 +6,27 @@ custom = TextClip(
             text="Welcome to my video !",
             font_size=48,
             color='#FFFFFF',
-            bg_color='#FFC0CB'
-        ) .with_duration(5).with_position('left')
+            bg_color='#FFC0CB',
+            size=(1300, 750)
+        ) .with_start(0).with_duration(5).with_position('left')
         
-bg_clip = ColorClip(size=(1300, 750) ,color=(255, 192, 203)).with_duration(5)
-
-custom = CompositeVideoClip([bg_clip,custom]).with_position('left')
+custom = CompositeVideoClip([custom]).with_position('left')
 v1 = VideoFileClip("videos/video2.mp4")
 outro = TextClip(
             font="font/font.ttf",
             text="Thank you!",
             font_size=48,
             color='#FFFFFF',
-            bg_color='#FFC0CB'
-        ) .with_duration(5).with_position('center')
+            bg_color='#FFC0CB',
+            size=(1300, 750)
+        ) .with_start(0).with_duration(5).with_position('center')
         
-bg_clip = ColorClip(size=(1300, 750) ,color=(255, 192, 203)).with_duration(5)
+outro = CompositeVideoClip([outro]).with_position('center')
 
-outro = CompositeVideoClip([bg_clip,outro]).with_position('center')
-layer_0 = concatenate_videoclips([
-    custom,
-    v1,
-    outro,
-], method="compose")
+v1=v1.with_start(custom.end)
+outro=outro.with_start(v1.end)
+layer1 = CompositeVideoClip([custom,v1,outro])
 
 
-final_video = layer_0
-final_video.write_videofile("my_video.mp4", fps=24)
+final_video = layer1
+final_video.write_videofile("./my_video.mp4", fps=24)
--- demo/generated/transition.py
+++ demo/generated/transition.py
@@ -5,11 +5,10 @@ v1 = VideoFileClip("videos/Video1.mp4")
 v1 = v1.with_effects([vfx.CrossFadeOut(1)])
 v2 = VideoFileClip("videos/Video2.mp4")
 v2 = v2.with_effects([vfx.CrossFadeIn(1)])
-layer1 = concatenate_videoclips([
-    v1,
-    v2,
-], method="compose")
+
+v2=v2.with_start(v1.end)
+layer1 = CompositeVideoClip([v1,v2])
 
 
 final_video = layer1
-final_video.write_videofile("my_video.mp4", fps=24)
+final_video.write_videofile("./my_video.mp4", fps=24)
